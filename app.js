c9e4bb4b14800f94258dfe3b82d2c135d60fa435
AkashB07 <akashbadhi7@gmail.com>
2022-11-10 12:28:01 +0530

    retriving data

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const express = require('express');
-
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
@@ -14,8 +13,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-db.execute('SELECT * FROM products');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
